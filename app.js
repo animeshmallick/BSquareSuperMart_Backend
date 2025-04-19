463c6f89084f7fb5d652c04153a8ee546b192a76
Subhrajeet <prakashparijat@gmail.com>
2025-04-19 13:22:16 +0530

    Category Router

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const fs = require('fs');
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
 
 const categories = require('./routes/categories');
+const category=require('./routes/category');
 
 const app = express();
 
@@ -27,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 logger.info('Server Started at port : ' + process.env.PORT || '3000')
 app.use('/categories', categories);
+app.use('/category', category);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/category.js
+++ routes/category.js
@@ -1,18 +1,21 @@
 const database = require('../internal/database.js')
 const parseCategories = require('../helpers/categoryHelper.js');
 const Sql = require('../internal/sql.js');
+const get_products_from_category_query= require('../internal/sql.js');
 const express = require('express');
 
 const router = express.Router();
 
-router.get('/', function (req, res, next){
-    const db = database();
-    db.query(Sql.get_all_categories(), (err, result) => {
+
+router.get('/:category', function (req, res, next){
+    const category=req.params.category;
+    const db= database();
+    db.query(Sql.get_products_from_category(category), (err, result) => {
         if(err){
             res.status(500).json({error: err.message});
             return;
         }
-        res.status(200).json(parseCategories(result));
+        res.status(200).json(result);
     });
     db.end();
 });
