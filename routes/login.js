5cd3a1af3ea2b46b8aaf54996539d2fa29544b23
= <animesh7995@gmail.com>
2025-05-26 21:05:27 +0530

    fix login

--- routes/login.js
+++ routes/login.js
@@ -14,13 +14,13 @@ router.post('/', function (req, res, next) {
         if (err) {
             res.status(500).json({error: err.message});
         }
-        console.log(result[0].userid);
         if (result.length === 1 && result[0].userid != null){
             authToken = Token.getToken(result[0].userid);
+            console.log(result[0].userid);
             res.status(200).json({authToken: authToken})
+        }else{
+            res.status(401).json({error: "No User Details Found"});
         }
-
-
     });
 
 });
