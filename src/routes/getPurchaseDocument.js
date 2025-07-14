d126873bf8b19161581c38044802711376e5d0fc
= <animesh7995@gmail.com>
2025-07-14 23:48:46 +0530

    fix to enable admin to get purchase doc

--- src/routes/getPurchaseDocument.js
+++ src/routes/getPurchaseDocument.js
@@ -51,7 +51,46 @@ router.get("/:purchaseID", token.verifyAuthToken, (req, res, next) => {
         })
         .catch(err => {
             res.status(500).json({error: err.message});
+        });
+});
+router.get("/admin/:purchaseID", token.verifyAdminAuthToken, (req, res, next) => {
+    const purchaseID = req.params.purchaseID;
+    let purchase_doc = {
+        signed: false,
+        purchase_id: purchaseID
+    };
+    database.query(Sql.get_purchase_details(purchaseID))
+        .then(async sql_response => {
+            if (sql_response.length === 1) {
+                purchase_doc.customer_id = sql_response[0].customer_id;
+                purchase_doc.signed = true;
+
+                purchase_doc.status = sql_response[0].status;
+                purchase_doc.address = sql_response[0].address_id;
+                purchase_doc.orders = sql_response[0].order_id;
+                purchase_doc.payment = sql_response[0].payment_id
+                purchase_doc.purchased_at = sql_response[0].placed_on;
+
+                purchase_doc.address = await getPurchaseDocHelper.getAddress(purchase_doc.address);
+                purchase_doc.payment = await getPurchaseDocHelper.getPayment(purchase_doc.payment);
+                purchase_doc.orders = await getPurchaseDocHelper.getOrders(purchase_doc.orders);
+
+                let allProductIds = purchase_doc.orders.map(order => order.product_id);
+                let all_products = await database.query(Sql.get_all_products_from_ids(allProductIds));
+
+                purchase_doc.orders.forEach(order => {
+                    order.product = getPurchaseDocHelper.findProduct(order.product_id, all_products);
+                    delete order.product_id;
+                });
+
+                res.status(200).json(purchase_doc);
+            } else {
+                res.status(400).json({error: "Invalid Purchase ID"});
+            }
         })
-})
+        .catch(err => {
+            res.status(500).json({error: err.message});
+        });
+});
 
 module.exports = router;
